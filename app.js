d098fbf14cb48dd4442ce61a53e9184823ec1918
Kaic Rodrigues de Souza <kaicrodrigues602@gmail.com>
2025-03-16 15:10:00 -0300

    adicionar nomes a lista, atualizar a lista de amigos, sortear lista de amigos

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-let nomesAmigos = []
+let nomesAmigos = [];
+
+function adicionarAmigos()
+{
+    const nome = document.getElementById('amigo');
+    if(nome === '')
+    {
+        alert('Por favor, insira um nome');
+    }
+    else
+    {
+        nomesAmigos.push(nome);
+        document.getElementById('amigo').value = '';
+        atualizarListaAmigos();
+    }
+
+    function atualizarListaAmigos()
+    {
+        const listaAmigos = document.getElementById('listaAmigos');
+
+        listaAmigos.innerHTML = '';
+
+        nomesAmigos.forEach(function(amigo)
+        {
+            const li = document.createElement('li');
+            listaAmigos.appendChild(li); 
+        });
+    }
+
+    function sorterAmigo()
+    {
+        if(nomesAmigos.length === 0)
+        {
+            document.getElementById('resultado').innerHTML = 'Não há amigos cadastrados para sortear.';
+            return ;           
+        }
+        else
+        {
+            const indiceAleatorio = Math.floor(Math.random() * nomesAmigos.length);
+            const  amigoSorteado = nomesAmigos[indiceAleatorio];
+            document.getElementById('resultado').innerHTML = `O amigo secreto é ${amigoSorteado}`;
+        }
+    }
+}
